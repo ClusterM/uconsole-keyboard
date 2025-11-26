2730f41a0853e6af37530a52d33765a7e3e7d571
Alexey Cluster <cluster@cluster.wtf>
2025-11-26 22:05:32 +0100

    Implement keyboard backlight resume functionality via trackball movement
    Signed-off-by: Alexey Cluster <cluster@cluster.wtf>

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -3,7 +3,14 @@
 
 #include <stdint.h>
 
+// The values for the keyboard backlight PWM (maximum value is 2000)
+static const uint16_t backlight_vals[3] = {0, 500, 2000};
+
+// The initial value of the keyboard backlight (index of the backlight_vals array)
+#define KEYBOARD_INITIAL_BACKLIGHT_VALUE_ID 0
+
 // The time of inactivity in seconds after which the keyboard backlight will turn off
+// 0 = never turn off
 #define KEYBOARD_BACKLIGHT_OFF_TIME 15
 
 // The duration of the keyboard backlight dim out effect in milliseconds
@@ -12,10 +19,7 @@
 // The value of the keyboard backlight when it is dimmed out
 #define KEYBOARD_BACKLIGHT_DIMMED_OUT_VALUE 0
 
-// The values for the keyboard backlight PWM (maximum value is 2000)
-static const uint16_t backlight_vals[3] = {0, 500, 2000};
-
-// The initial value of the keyboard backlight (index of the backlight_vals array)
-#define KEYBOARD_INITIAL_BACKLIGHT_VALUE_ID 0
+// If defined, the keyboard backlight will resume when the trackball is moved
+#define KEYBOARD_BACKLIGHT_RESUME_BY_TRACKBALL
 
 #endif
